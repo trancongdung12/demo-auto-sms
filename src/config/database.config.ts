e29576105e3f8b015f509d355614623455fdb222
dung tran <trancongdung12@gmail.com>
2025-03-23 11:31:48 +0700

    add db config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,12 +2,12 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-//   url: process.env.DATABASE_URL,
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'dray112000',
-  database: process.env.DB_NAME || 'auto_sms_db',
+  url: 'postgres://neondb_owner:npg_Pp6Mzu9fsXLN@ep-frosty-salad-a1859z4b-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require',
+  host:process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: process.env.NODE_ENV !== 'production', // Only true in development
   ssl: process.env.NODE_ENV === 'production' 
